9a1ef141f9cdff6ce5bcdbebdb47986b5db8c485
水峰 <shangxuebin1991@gmail.com>
2020-03-25 01:33:25 +0800

    code optimize

--- lib/beginner/border_radius_previewer.dart
+++ lib/beginner/border_radius_previewer.dart
@@ -19,58 +19,51 @@ class BorderRadiusPreviewerPage extends StatefulWidget {
 }
 
 class _BorderRadiusPreviewerPageState extends State<BorderRadiusPreviewerPage> {
-  Radius border_top_left_radius = Radius.zero;
-  Radius border_top_right_radius = Radius.zero;
-  Radius border_bottom_right_radius = Radius.zero;
-  Radius border_bottom_left_radius = Radius.zero;
 
-  List<Function> controllerActions;
-  List<TextEditingController> controllers;
+  BorderStyle _borderStyle;
+  List<TextEditingController> _controllers;
 
   @override
   void initState() {
     super.initState();
-    controllers = List.generate(8, (i){
+    _borderStyle = BorderStyle();
+    _controllers = List.generate(8, (i) {
       final controller = TextEditingController();
-      controller.addListener((){
-        controllerActions[i](double.tryParse(controller.text)??0.0);
+      controller.addListener(() {
+        setState(() {
+          _borderStyle[i] = double.tryParse(controller.text)??0;
+        });
       });
       return controller;
-    },growable: false);
-    controllerActions = [
-    (r)=>setState((){border_top_left_radius = Radius.elliptical(r,border_top_left_radius.y);}),
-    (r)=>setState((){border_top_left_radius = Radius.elliptical(border_top_left_radius.x,r);}),
-    (r)=>setState((){{border_top_right_radius = Radius.elliptical(border_top_right_radius.x,r);}}),
-    (r)=>setState((){border_top_right_radius = Radius.elliptical(r,border_top_right_radius.y);}),
-    (r)=>setState((){border_bottom_right_radius=Radius.elliptical(r,border_bottom_right_radius.y);}),
-    (r)=>setState((){border_bottom_right_radius=Radius.elliptical(border_bottom_right_radius.x, r);}),
-    (r)=>setState((){{border_bottom_left_radius=Radius.elliptical(border_bottom_left_radius.x, r);}}),
-    (r)=>setState((){{border_bottom_left_radius=Radius.elliptical(r, border_bottom_left_radius.y);}}),
-    ];
+    }, growable: false);
+  }
+
+  @override
+  void dispose() {
+    _controllers.forEach((c)=>c.dispose());
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final css = """
-border-top-left-radius:${border_top_left_radius.x}px / ${border_top_left_radius.y}px;
-border-top-right-radius:${border_top_right_radius.x}px / ${border_top_right_radius.y}px;
-border-bottom-right-radius:${border_bottom_right_radius.x}px / ${border_bottom_right_radius.y}px;
-border-bottom-left-radius:${border_bottom_left_radius.x}px / ${border_bottom_left_radius.y}px;
-""";
-    final edits = controllers.map((c)=>SizedBox(
-      width: 60,
-      height: 40,
-      child: TextField(
-        controller: c,
-        decoration: InputDecoration(border: OutlineInputBorder()),
-        keyboardType: TextInputType.numberWithOptions(
-          signed: false,
-        ),
-      ),
-    )).toList();
+    final edits = _controllers
+        .map((c) =>
+        SizedBox(
+          width: 60,
+          height: 40,
+          child: TextField(
+            controller: c,
+            decoration: InputDecoration(border: OutlineInputBorder()),
+            keyboardType: TextInputType.numberWithOptions(
+              signed: false,
+            ),
+          ),
+        ))
+        .toList();
 
     return Column(
       children: <Widget>[
+        SizedBox(height: 10,),
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -78,10 +71,7 @@ border-bottom-left-radius:${border_bottom_left_radius.x}px / ${border_bottom_lef
               width: 200,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  edits[1],
-                  edits[2]
-                ],
+                children: <Widget>[edits[1], edits[2]],
               ),
             ),
             Row(
@@ -91,57 +81,127 @@ border-bottom-left-radius:${border_bottom_left_radius.x}px / ${border_bottom_lef
                   height: 200,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      edits[0],
-                      edits[7]
-                    ],
+                    children: <Widget>[edits[0], edits[7]],
                   ),
                 ),
-               Container(
-                 width: 200,
-                 height: 200,
-                 child:  Container(
-                   margin: EdgeInsets.all(2),
-                   decoration: BoxDecoration(
-                     color: Colors.green,
-                     borderRadius: BorderRadius.only(
-                       topLeft: border_top_left_radius,
-                       topRight: border_top_right_radius,
-                       bottomRight: border_bottom_right_radius,
-                       bottomLeft: border_bottom_left_radius,
-                     ),
-                   ),
-                 ),
-               ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  child: Container(
+                    margin: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: _borderStyle.borderRadius,
+                    ),
+                  ),
+                ),
                 SizedBox(
                   height: 200,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      edits[3],
-                      edits[4]
-                    ],
+                    children: <Widget>[edits[3], edits[4]],
                   ),
                 ),
               ],
             ),
-           SizedBox(
-             width: 200,
-             child:  Row(
-               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-               children: <Widget>[
-                 edits[6],
-                 edits[5]
-               ],
-             ),
-           ),
+            SizedBox(
+              width: 200,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[edits[6], edits[5]],
+              ),
+            ),
           ],
         ),
         Padding(
           padding: EdgeInsets.only(top: 20),
-          child: SelectableText(css),
+          child: SelectableText(_borderStyle.cssString),
         )
       ],
     );
   }
+}
+
+class BorderStyle {
+
+  Radius border_top_left_radius = Radius.zero;
+  Radius border_top_right_radius = Radius.zero;
+  Radius border_bottom_right_radius = Radius.zero;
+  Radius border_bottom_left_radius = Radius.zero;
+
+  void operator []=(int index, double value) {
+    switch (index) {
+      case 0:
+        border_top_left_radius =
+            Radius.elliptical(value, border_top_left_radius.y);
+        break;
+      case 1:
+        border_top_left_radius =
+            Radius.elliptical(border_top_left_radius.x, value);
+        break;
+      case 2:
+        border_top_right_radius =
+            Radius.elliptical(border_top_right_radius.x, value);
+        break;
+      case 3:
+        border_top_right_radius =
+            Radius.elliptical(value, border_top_right_radius.y);
+        break;
+      case 4:
+        border_bottom_right_radius =
+            Radius.elliptical(value, border_bottom_right_radius.y);
+        break;
+      case 5:
+        border_bottom_right_radius =
+            Radius.elliptical(border_bottom_right_radius.x, value);
+        break;
+      case 6:
+        border_bottom_left_radius =
+            Radius.elliptical(border_bottom_left_radius.x, value);
+        break;
+      case 7:
+        border_bottom_left_radius =
+            Radius.elliptical(value, border_bottom_left_radius.y);
+        break;
+    }
+  }
+
+  double operator [](int index) {
+    switch (index) {
+      case 0:
+        return border_top_left_radius.x;
+      case 1:
+        return border_top_left_radius.y;
+      case 2:
+        return border_top_right_radius.y;
+      case 3:
+        return border_top_right_radius.x;
+      case 4:
+        return border_bottom_right_radius.x;
+      case 5:
+        return border_bottom_right_radius.y;
+      case 6:
+        return border_bottom_left_radius.y;
+      case 7:
+        return border_bottom_left_radius.x;
+    }
+  }
+
+  String get cssString => """
+border-top-left-radius:${border_top_left_radius.x}px / ${border_top_left_radius
+      .y}px;
+border-top-right-radius:${border_top_right_radius
+      .x}px / ${border_top_right_radius.y}px;
+border-bottom-right-radius:${border_bottom_right_radius
+      .x}px / ${border_bottom_right_radius.y}px;
+border-bottom-left-radius:${border_bottom_left_radius
+      .x}px / ${border_bottom_left_radius.y}px;
+""";
+
+  BorderRadius get borderRadius => BorderRadius.only(
+    topLeft: border_top_left_radius,
+    topRight: border_top_right_radius,
+    bottomRight: border_bottom_right_radius,
+    bottomLeft: border_bottom_left_radius,
+  );
 }
